e4b8ec6120ac5e18137429aac499f83c613e19be
Jay Merrifield <fracmak@gmail.com>
2011-09-19 00:19:45 -0400

    whitespace cleanup

--- modules/tv/tmpl/default/list_data.php
+++ modules/tv/tmpl/default/list_data.php
@@ -44,11 +44,11 @@
         $timeslot_anchor    = 0;
         $channel_count      = 0;
         $displayed_channels = array();
-        $timeslots_left		= 0;
-        $timeslots_used		= 0;
+        $timeslots_left     = 0;
+        $timeslots_used     = 0;
 
-		$last_chan_id		= -1;
-		// this loads a program list, sorted by channel, then start time
+        $last_chan_id       = -1;
+        // this loads a program list, sorted by channel, then start time
         $program_list = load_program_list($list_starttime, $list_endtime);
 
         foreach ($program_list as $program) {
@@ -60,83 +60,83 @@
             if ($program_ends > $list_endtime)
                 $program_ends = $list_endtime;
         // check to see if we've moved onto a new channel row
-        	if ($last_chan_id != $program->chanid){
-		        // Ignore channels with no number
-        	    if (strlen($program->channum) < 1)
-            	    continue;
-		        // Skip already-displayed channels
-        	    if ($displayed_channels[$program->channum][$program->callsign])
-            	    continue;
-            	$displayed_channels[$program->channum][$program->callsign] = 1;
+            if ($last_chan_id != $program->chanid){
+                // Ignore channels with no number
+                if (strlen($program->channum) < 1)
+                    continue;
+                // Skip already-displayed channels
+                if ($displayed_channels[$program->channum][$program->callsign])
+                    continue;
+                $displayed_channels[$program->channum][$program->callsign] = 1;
 
-        		if ($last_chan_id != -1){
-        		// close the row, first rows have no rows to close
-				// Uh oh, there are leftover timeslots - display a no data message
-					if ($timeslots_left > 0) {
-						$timeslots_used = $timeslots_left;
-						require tmpl_dir.'list_cell_nodata.php';
-					}
-        			echo '<td>&nbsp;</td></tr>';
-				}
-        		$last_chan_id = $program->chanid;
-		        // Display the timeslot bar?
-	            if ($channel_count % timeslotbar_skip == 0) {
-		            // Update the timeslot anchor
-        	        $timeslot_anchor++;
+                if ($last_chan_id != -1){
+                // close the row, first rows have no rows to close
+                // Uh oh, there are leftover timeslots - display a no data message
+                    if ($timeslots_left > 0) {
+                        $timeslots_used = $timeslots_left;
+                        require tmpl_dir.'list_cell_nodata.php';
+                    }
+                    echo '<td>&nbsp;</td></tr>';
+                }
+                $last_chan_id = $program->chanid;
+                // Display the timeslot bar?
+                if ($channel_count % timeslotbar_skip == 0) {
+                    // Update the timeslot anchor
+                    $timeslot_anchor++;
 ?><tr>
     <td class="menu" align="right"><a class="link" onclick="list_update(<?php echo $list_starttime - (timeslot_size * num_time_slots); ?>);" name="anchor<?php echo $timeslot_anchor ?>"><img src="<?php echo skin_url ?>img/left.gif" alt="left"></a></td>
 <?php
-	                $block_count = 0;
-    	            foreach ($Timeslots as $time) {
-        	            if ($block_count++ % timeslot_blocks)
-            	            continue;
+                    $block_count = 0;
+                    foreach ($Timeslots as $time) {
+                        if ($block_count++ % timeslot_blocks)
+                            continue;
 ?>
     <td class="menu nowrap" colspan="<?php echo timeslot_blocks ?>" align="center"><a class="link" onclick="list_update(<?php echo $time; ?>);"><?php echo strftime($_SESSION['time_format'], $time) ?></a></td>
 <?php
-	                }
+                    }
 ?>
     <td class="menu nowrap"><a class="link" onclick="list_update(<?php echo $list_starttime + (timeslot_size * num_time_slots); ?>);"><img src="<?php echo skin_url ?>img/right.gif" alt="right"></a></td>
 </tr><?php
-	            }
-		        // Count this channel
-	            $channel_count++;
-			// Print the data
-	?><tr>
-		<td class="x-channel">
-			<a href="<?php echo root_url ?>tv/channel/<?php echo $program->chanid, '/', $list_starttime ?>"
-					title="<?php
-						echo t('Details for: $1',
-							   html_entities($program->channame).'; '.$program->channum)
-					?>">
-	<?php       if ($_SESSION["show_channel_icons"] == true && !empty($program->chanicon)) { ?>
-			<img src="<?php echo $program->chanicon ?>" style="padding:5px;"><br>
-	<?php       } ?>
-			<?php echo ($_SESSION["prefer_channum"] ? $program->channum : $program->callsign), "\n" ?>
-	<?php       if ($_SESSION["show_channel_icons"] == false || empty($program->chanicon)) {
-					echo '<br>('.($_SESSION["prefer_channum"] ? $program->callsign : $program->channum), ")\n";
-	} ?>
-			</a>
-			</td>
-	<?php
-				if (defined('theme_num_time_slots')) {
-					$timeslots_left = theme_num_time_slots;
-					$timeslot_size = theme_timeslot_size;
-				} else {
-					$timeslots_left = num_time_slots;
-					$timeslot_size = timeslot_size;
-				}
-				if ($program_starts > $list_starttime) {
-					$length = (($program_starts - $list_starttime) / $timeslot_size);
-					if ($length >= 0.5) {
-						$timeslots_used = ceil($length);
-						require tmpl_dir.'list_cell_nodata.php';
-						$timeslots_left -= $timeslots_used;
-					}
-				}
-	        } // end new channel
+                }
+                // Count this channel
+                $channel_count++;
+            // Print the data
+    ?><tr>
+        <td class="x-channel">
+            <a href="<?php echo root_url ?>tv/channel/<?php echo $program->chanid, '/', $list_starttime ?>"
+                    title="<?php
+                        echo t('Details for: $1',
+                               html_entities($program->channame).'; '.$program->channum)
+                    ?>">
+    <?php       if ($_SESSION["show_channel_icons"] == true && !empty($program->chanicon)) { ?>
+            <img src="<?php echo $program->chanicon ?>" style="padding:5px;"><br>
+    <?php       } ?>
+            <?php echo ($_SESSION["prefer_channum"] ? $program->channum : $program->callsign), "\n" ?>
+    <?php       if ($_SESSION["show_channel_icons"] == false || empty($program->chanicon)) {
+                    echo '<br>('.($_SESSION["prefer_channum"] ? $program->callsign : $program->channum), ")\n";
+    } ?>
+            </a>
+            </td>
+    <?php
+                if (defined('theme_num_time_slots')) {
+                    $timeslots_left = theme_num_time_slots;
+                    $timeslot_size = theme_timeslot_size;
+                } else {
+                    $timeslots_left = num_time_slots;
+                    $timeslot_size = timeslot_size;
+                }
+                if ($program_starts > $list_starttime) {
+                    $length = (($program_starts - $list_starttime) / $timeslot_size);
+                    if ($length >= 0.5) {
+                        $timeslots_used = ceil($length);
+                        require tmpl_dir.'list_cell_nodata.php';
+                        $timeslots_left -= $timeslots_used;
+                    }
+                }
+            } // end new channel
 
         // Calculate the number of time slots this program gets
-        	$length = round($program_ends / $timeslot_size) - round($program_starts / $timeslot_size);
+            $length = round($program_ends / $timeslot_size) - round($program_starts / $timeslot_size);
             if ($length < .5) continue; // ignore shows that don't take up at least half a timeslot
             $timeslots_used = round($length);
             $timeslots_left -= $timeslots_used;
